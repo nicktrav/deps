170e1593be1c5edc2f66721a4814a70d698b373d
Nick Travers <n.e.travers@gmail.com>
2018-10-29 00:14:07 +0000

    Add dep for openssl
    Compile curl against local OpenSSL.

---
+++ openssl.rb
@@ -0,0 +1,22 @@
+dep 'openssl', :version do
+  requires 'make.apt'
+  version.default!('1.1.1')
+  met? { shell? "openssl version | grep -E '^OpenSSL #{version}'" }
+  meet {
+    cd '/tmp' do
+      shell 'rm -rf openssl'
+      shell 'git clone https://github.com/openssl/openssl.git'
+      cd 'openssl' do
+        shell "git checkout OpenSSL_#{version.to_s.gsub('.', '_')}"
+        shell "./config --prefix=/usr/local/ssl --openssldir=/usr/local/ssl '-Wl,-rpath,\$(LIBRPATH)'"
+        shell 'make'
+        shell 'make install', sudo: true
+        shell 'cp apps/openssl /usr/local/bin/', sudo: true
+      end
+    end
+  }
+  after do
+    shell 'rm -rf /tmp/openssl'
+    # TODO(nickt): Remove system openssl here too?
+  end
+end
