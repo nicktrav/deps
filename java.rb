7b6c64622d146b39cfa25c47aca85c38d28c2534
Nick Travers <n.e.travers@gmail.com>
2018-12-09 01:12:01 +0000

    [debian] Use Jabba

--- java.rb
+++ java.rb
@@ -1,14 +1,13 @@
-dep 'java', :version, :patch  do
-  requires 'curl', 'tar.apt'
-  met? { shell? "java -version 2>&1 | grep 11.0.1" }
+dep 'openjdk.bash', :version do
+  version.default! 'openjdk@1.11.0-1'
+  requires 'jabba', 'dotfiles'
+  met? { login_shell "which jabba && jabba current | grep #{version}" }
   meet {
-    url="https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz"
-    shell <<-HERE
-      cd /tmp && rm -rf java* openjdk* && \
-      curl -L -o java.tar.gz #{url} && \
-      sudo tar xzf java.tar.gz -C /var/lib && \
-      sudo ln -s /var/lib/jdk-11.0.1/bin/java /usr/local/bin/java && \
-      rm -rf java* openjdk*
-    HERE
+    login_shell "jabba install #{version}"
+    login_shell "jabba alias default #{version}"
   }
+end
+
+dep 'java' do
+  requires 'openjdk.bash'
 end
