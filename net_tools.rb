5a71addf6402905ddc31cc13d947251e56a70402
Nick Travers <n.e.travers@gmail.com>
2018-12-17 00:00:57 +0000

    Manually install net-tools via apt

--- net_tools.rb
+++ net_tools.rb
@@ -1,5 +1,6 @@
-dep 'net-tools.lib', :version do
-  installs {
-    via :apt, 'net-tools'
+dep 'net-tools', :version do
+  met? { shell? 'dpkg -s net-tools' }
+  meet {
+    shell 'apt-get install net-tools', sudo: true
   }
 end
