e57a9547490158bc759d3e895cc548b4f3b0d97c
Nick Travers <n.e.travers@gmail.com>
2018-12-24 04:24:40 +0000

    Update Rust version to 1.31.1

--- rust.rb
+++ rust.rb
@@ -30,7 +30,7 @@ end
 
 dep 'rustc.rust', :version do
   requires 'rustup.rust', 'dotfiles'
-  version.default!('1.31.0')
+  version.default!('1.31.1')
   met? { shell? "#{rustc} --version | grep #{version}" }
   meet { shell "#{rustup} update #{version} && #{rustup} default #{version}" }
 end
