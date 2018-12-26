4a218af1865b6bc9da30244243f77592ba6b761b
Nick Travers <n.e.travers@gmail.com>
2018-12-26 08:24:04 +0000

    Use sudo when adding to sources.list

--- debian_backports.rb
+++ debian_backports.rb
@@ -1,7 +1,7 @@
 dep 'debian-backports'  do
   met? { shell 'cat /etc/apt/sources.list | grep stretch-backports' }
   meet {
-    shell "echo 'deb http://deb.debian.org/debian stretch-backports main' >> /etc/apt/sources.list"
+    shell "echo 'deb http://deb.debian.org/debian stretch-backports main' >> /etc/apt/sources.list", sudo: true
   }
   after {
     shell 'apt-get update', sudo: true
