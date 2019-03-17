c48167400d28c69fd448cd1136d3cc6da0ed94b2
Nick Travers <n.e.travers@gmail.com>
2019-03-17 08:57:23 -0700

    Update Java to OpenJDK 1.11.0-2

--- java.rb
+++ java.rb
@@ -1,5 +1,5 @@
 dep 'openjdk.bash', :version do
-  version.default! 'openjdk@1.10.0'
+  version.default! 'openjdk@1.11.0-2'
   requires 'jabba', 'dotfiles'
   met? { login_shell "jabba current | grep #{version}" }
   meet {
