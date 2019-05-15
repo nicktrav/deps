8644d971ec715e6870b367100c0be6d8e8f0a5ff
Nick Travers <n.e.travers@gmail.com>
2019-05-14 22:09:34 -0700

    Bump Rust version to 1.34.2
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

--- rust.rb
+++ rust.rb
@@ -30,7 +30,7 @@ end
 
 dep 'rustc.rust', :version do
   requires 'rustup.rust', 'dotfiles'
-  version.default!('1.34.1')
+  version.default!('1.34.2')
   met? { shell? "#{rustc} --version | grep #{version}" }
   meet { shell "#{rustup} update #{version} && #{rustup} default #{version}" }
 end
