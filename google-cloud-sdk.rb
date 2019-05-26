1b3a7e7b62fd99caa68ece50c363480f3c163c8c
Nick Travers <n.e.travers@gmail.com>
2019-05-26 16:56:05 -0700

    Add google-cloud-sdk
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

---
+++ google-cloud-sdk.rb
@@ -0,0 +1,41 @@
+meta :gcloud do
+  def dir
+    '~/google-cloud-sdk'
+  end
+end
+
+dep 'dir.gcloud' do
+  met? { dir.p.exists? }
+  meet {
+    shell "mkdir #{dir}"
+  }
+end
+
+dep 'google-cloud-sdk.gcloud', :version do
+  version.default!('247.0.0')
+  requires 'curl', 'dir.gcloud', 'tar.bin'
+
+  url_base="https://dl.google.com/dl/cloudsdk/channels/rapid/downloads"
+  url=''
+  on :debian do
+    url="#{url_base}/google-cloud-sdk-#{version}-linux-x86_64.tar.gz"
+  end
+  on :osx do
+    url="#{url_base}/google-cloud-sdk-#{version}-darwin-x86_64.tar.gz"
+  end
+
+  met? { login_shell "gcloud --version | grep #{version}" }
+  meet {
+    cd dir do
+      shell "curl -o google-cloud-sdk.tar.gz -L #{url}"
+      shell 'tar xzf google-cloud-sdk.tar.gz --strip 1'
+    end
+  }
+  after {
+    shell "rm -rf #{dir}/google-cloud-sdk.tar.gz"
+  }
+end
+
+dep 'google-cloud-sdk', :version do
+  requires 'google-cloud-sdk.gcloud'
+end
