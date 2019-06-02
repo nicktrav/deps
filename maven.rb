f3435e10f779e768ac7f9ced1d47ccec33201a33
Nick Travers <n.e.travers@gmail.com>
2019-06-01 21:29:11 -0700

    Bump maven to 3.6.1
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

--- maven.rb
+++ maven.rb
@@ -1,6 +1,6 @@
 dep 'maven', :version  do
   requires 'curl', 'dotfiles', 'java'
-  version.default!('3.6.0')
+  version.default!('3.6.1')
   met? { login_shell "mvn --version | grep #{version}" }
   meet {
     url="https://archive.apache.org/dist/maven/maven-3/#{version}/binaries/apache-maven-#{version}-bin.tar.gz"
@@ -9,9 +9,9 @@ dep 'maven', :version  do
     # TODO(nickt): verify the sig
     cd '/tmp' do
       shell "curl -L -o #{tarball} #{url}"
-      shell "mkdir -p #{install_dir}", sudo: true
-      shell "tar xzf #{tarball} -C #{install_dir} --strip 1", sudo: true
-      shell "ln -s #{install_dir}/bin/mvn /usr/local/bin/mvn", sudo: true
+      shell "mkdir -p #{install_dir}"
+      shell "tar xzf #{tarball} -C #{install_dir} --strip 1"
+      shell "ln -s #{install_dir}/bin/mvn /usr/local/bin/mvn"
     end
   }
   after {
