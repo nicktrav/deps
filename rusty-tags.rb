665e4db471c2211dca781dd4e1095f2a183b753b
Nick Travers <n.e.travers@gmail.com>
2019-06-01 21:43:36 -0700

    Update rusty-tags to 3.5.1
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

--- rusty-tags.rb
+++ rusty-tags.rb
@@ -1,6 +1,6 @@
 dep 'rusty-tags.rust', :version do
   requires 'rustup.rust', 'dotfiles'
-  version.default!('3.2.0')
+  version.default!('3.5.1')
   met? { shell? "#{cargo} install --list | grep 'rusty-tags v#{version}'" }
   meet { shell "#{cargo} install --force --version #{version} rusty-tags" }
 end
