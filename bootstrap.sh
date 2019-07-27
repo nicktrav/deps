ccb11765434b55d929a5ad670e590444e8fa57ce
Nick Travers <n.e.travers@gmail.com>
2019-07-27 13:13:48 -0700

    Port to matryoshka
    Port all existing deps to use matryoshka.
    
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,17 +2,20 @@
 
 set -euo pipefail
 
-# install Ruby
-sudo apt-get -y update
-sudo apt-get -y install ruby git
+# install Go
+apt-get -y update && apt-get -y install git wget tar
 
-# install and unpack babuska
-rm -rf /tmp/babushka
-git clone https://github.com/benhoskings/babushka.git /tmp/babushka
-cd /tmp/babushka || exit 1
-sudo ./bin/babushka.rb babushka
+wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
+tar xzf go1.12.6.linux-amd64.tar.gz
+mv go /usr/local/
+
+# install matryoshka
+rm -rf /tmp/matryoshka
+git clone https://github.com/nicktrav/matryoshka.git /tmp/matryoshka
+cd /tmp/matryoshka || exit 1
+go build -o /usr/local/bin/matryoshka ./cmd/matryoshka/matryoshka.go
 
 # set up deps
-rm -rf ~/.babushka/deps
-mkdir -p ~/.babushka && cd ~/.babushka && git clone https://github.com/nicktrav/deps.git
-babushka --debug all
+rm -rf ~/.config/matryoshka/deps
+mkdir -p ~/.config/matryoshka && cd ~/.config/matryoshka && git clone https://github.com/nicktrav/deps.git
+matryoshka -dir ~/.config/matryoshka/deps -debug
